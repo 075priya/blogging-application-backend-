01624a50dd9154afb51da3a3787bcdee067dc055
075priya <priyadharshini.6761@gmail.com>
2024-03-11 01:49:38 -0700

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 const app = express();
 const PORT = 4000;
 
-const url = "mongodb+srv://priyadharshini6761:mongodbatlaspriya7@cluster0.75tigyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const url = "mongodb+srv://udhayakanan77:udhaya@cluster0.c6ldsco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(url);
 
@@ -25,4 +25,44 @@ app.post("/post", async function (req, res) {
 
   const POSTMETHOD = await client.db("CRUD").collection("DATA").insertMany([postMethod]);
   res.status(200).send(POSTMETHOD);
+});
+
+
+//---------------get method --------------------
+
+app.get("/get", async function (req, res) {
+  const GetMethod = await client.db("CRUD").collection("DATA").find({}).toArray();
+  res.status(200).send(GetMethod);
+});
+
+
+//---------single data get method -------------------------
+app.get("/get/:singleId", async function (req, res) {
+  const { singleId } = req.params;
+
+  const SingleIdGetMethod = await client.db("CRUD").collection("DATA").findOne({ _id: new ObjectId(singleId) });
+  res.status(200).send(SingleIdGetMethod);
+});
+
+//---------------------------------Update method-------------------------------------------------------------
+app.put("/update/:singlePut",  async function (req, res) {
+  const { singlePut } = req.params;
+  const UpdateMethod = req.body;
+  const singleId = await client.db("CRUD").collection("DATA").updateOne({ _id: new ObjectId(singlePut) }, { $set: UpdateMethod });
+
+  res.status(200).send(singleId);
+});
+
+//----------------------------Delete method -----------------
+
+app.delete("/delete/:deleteMethod", async function (req, res) {
+  const { deleteMethod } = req.params;
+
+  const DELETEMETHOD = await client.db("CRUD").collection("DATA").deleteOne({ _id: new ObjectId(deleteMethod) });
+
+  res.status(200).send(DELETEMETHOD);
+});
+
+app.listen(PORT, () => {
+  console.log("Server connected on port", PORT);
 });
